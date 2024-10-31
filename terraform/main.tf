b3f09b14ca2d11daff1a46d1a93557385ec4ba14
AdeSam97 <adejaresamson42@gmail.com>
2024-10-31 16:24:32 +0100

    updated cluster vm size

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_DS2_v3"
+    vm_size    = "standard_d2ps_v5"
   }
 
   identity {
